373715cc4790ca8a14cc8de73353c1c0b541b989
Douglas Prado <douglas.prado@forlogic.net>
2025-03-25 11:28:54 -0300

    Adiciona a pasta api ao projeto

---
+++ Nova pasta/Models/Tasks/TaskClass.cs
@@ -0,0 +1,13 @@
+﻿namespace todo.Models.Tasks
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int IdUser { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public bool Completed { get; set; }
+        public bool Removed { get; set; }
+    }
+}
---
+++ Nova pasta/Models/Users/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace todo.Models.Users
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<User>().Property(u => u.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            modelBuilder.Entity<User>().Property(u => u.Name).HasColumnName("name").HasMaxLength(500).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Email).HasColumnName("email").HasMaxLength(500).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Password).HasColumnName("password").HasMaxLength(300).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.CreationDate).HasColumnName("creation_date").HasColumnType("datetimeoffset").IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.IsSysAdmin).HasColumnName("is_sys_admin").HasColumnType("bit").IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Removed).HasColumnName("removed").HasColumnType("bit").IsRequired();
+        }
+    }
+}
